0b6ad132a685ce317a2ef2098a2562c6b4e3d690
hugodk33 <hugodk33@gmail.com>
2019-05-10 12:43:41 -0300

    Updates

---
+++ precache-manifest.3bcbd326ab489ea94a4275ef56da0885.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "b3719485eefecc1ee0b647027e6bf9af",
+    "url": "/hugoogleport/static/media/horse.b3719485.png"
+  },
+  {
+    "revision": "688666176136116324c8",
+    "url": "/hugoogleport/static/css/main.c15c9bf2.chunk.css"
+  },
+  {
+    "revision": "8dbcc72bbf5806c1289639c226358910",
+    "url": "/hugoogleport/static/media/thumb-soluthec-22.8dbcc72b.jpg"
+  },
+  {
+    "revision": "bfb915d3d4fe1df853a3",
+    "url": "/hugoogleport/static/js/1.bfb915d3.chunk.js"
+  },
+  {
+    "revision": "27acf9b70b9809f3bb11",
+    "url": "/hugoogleport/static/js/runtime~main.27acf9b7.js"
+  },
+  {
+    "revision": "f63da0f165574306df286601501b0aef",
+    "url": "/hugoogleport/static/media/BgMT.f63da0f1.jpg"
+  },
+  {
+    "revision": "688666176136116324c8",
+    "url": "/hugoogleport/static/js/main.68866617.chunk.js"
+  },
+  {
+    "revision": "059d9fc0cfecf152b90cf4b572449077",
+    "url": "/hugoogleport/static/media/thumb-uxui-4.059d9fc0.jpg"
+  },
+  {
+    "revision": "062de3beebe46b23942e9fc4884067eb",
+    "url": "/hugoogleport/static/media/thumb-bigdoor-11.062de3be.jpg"
+  },
+  {
+    "revision": "d93cab3a18b0cd64dcbd86f53a2bd5e5",
+    "url": "/hugoogleport/static/media/thumb-uxui-33.d93cab3a.jpg"
+  },
+  {
+    "revision": "bfb915d3d4fe1df853a3",
+    "url": "/hugoogleport/static/css/1.4147c89f.chunk.css"
+  },
+  {
+    "revision": "8b3c179e6e34f7cb2802594039939343",
+    "url": "/hugoogleport/index.html"
+  }
+];
